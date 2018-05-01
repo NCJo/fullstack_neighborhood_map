c5c9efec8c2af1a230ff95290a5c8c5d3fb0a144
NCJo <cnimkulr@gmail.com>
2018-05-01 00:30:38 -0600

    Added: Framework of navbar in main.html

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.608013, lng: -122.335167},
+    zoom: 15,
+    // Check 'googleMapStyling.js' for details
+    styles: styles,
+    mapTypeControl: false
+  });
+}
